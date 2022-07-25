ace32d09cb1632546bf6c1c65b8b983a852b1d8c
Gary Franco <gfranco@corp.dharbor.com>
2022-07-25 13:44:46 -0400

    gateway

--- src/main/java/com/onboarding/configservice/ConfigServiceApplication.java
+++ src/main/java/com/onboarding/configservice/ConfigServiceApplication.java
@@ -3,9 +3,7 @@ package com.onboarding.configservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.web.bind.annotation.CrossOrigin;
 
-@CrossOrigin(origins = "*")
 @SpringBootApplication
 @EnableConfigServer
 public class ConfigServiceApplication {
